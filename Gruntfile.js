c03a8b421be84703d594dd5f9a8d8f68b486bcdf
ForeverCc <1615250697@qq.com>
2017-04-27 22:37:49 +0800

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,50 +11,50 @@ module.exports = function(grunt){
                 banner: '/*! <%= pkg.name %><%= pkg.version %> \n' + 'author:xc'+ '*/\n'
             },
             dist: {
-				files: [
-					{
-						src: 'js/index.js',
-               			dest: 'dest/index.min.js'
-					},
-					{
-						src: 'js/mac.js',
-               			dest: 'dest/mac.min.js'
-					},
-					{
-						src: 'js/mobile.js',
-               			dest: 'dest/mobile.min.js'
-					}
-				]
+		files: [
+			{
+				src: 'js/index.js',
+				dest: 'dest/index.min.js'
+			},
+			{
+				src: 'js/mac.js',
+				dest: 'dest/mac.min.js'
+			},
+			{
+				src: 'js/mobile.js',
+				dest: 'dest/mobile.min.js'
+			}
+		]
                 
             }
         },
 		
-		cssmin: {
-			dist:{
-				files: [
-					{
-						src: 'css/index.css',
-               			dest: 'css/index.min.css'
-					},
-					{
-						src: 'css/mac.css',
-               			dest: 'css/mac.min.css'
-					},
-					{
-						src: 'css/mobile.css',
-               			dest: 'css/mobile.min.css'
-					},
-					{
-						src: 'css/global.css',
-               			dest: 'css/global.min.css'
-					}
-				]
-			}
-		
+	cssmin: {
+		dist:{
+			files: [
+				{
+					src: 'css/index.css',
+					dest: 'css/index.min.css'
+				},
+				{
+					src: 'css/mac.css',
+					dest: 'css/mac.min.css'
+				},
+				{
+					src: 'css/mobile.css',
+					dest: 'css/mobile.min.css'
+				},
+				{
+					src: 'css/global.css',
+					dest: 'css/global.min.css'
+				}
+			]
 		}
+
+	}
 
     });
     grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.registerTask('default', ['uglify', 'cssmin']);
 };
